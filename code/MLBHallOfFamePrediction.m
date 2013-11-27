46af0529319dabbb3bee11b92c95261fb49d79a6
unknown <jondavidford@gmail.com>
2013-11-27 21:30:51 +0000

    initial commit

---
+++ code/MLBHallOfFamePrediction.m
@@ -0,0 +1,10 @@
+function [] = MLBHallOfFamePrediction(trainingData,testingData)
+% MLBHallOfFamePrediction
+%
+% describe that stuff
+%
+% AUTHORS: Elliott Evans, Jon Ford, Corey McMahon
+
+    % body
+
+end
